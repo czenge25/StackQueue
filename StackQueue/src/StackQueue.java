baadfb5f3ac86e846445370d1d1312f9f1f7a711
czenge25 <czenge25@kentdenver.org>
2023-11-29 10:55:10 -0700

    Updated enqueue method

--- StackQueue/src/StackQueue.java
+++ StackQueue/src/StackQueue.java
@@ -1,2 +1,23 @@
-public class StackQueue {
+public class StackQueue<T> {
+
+    Stack s1;
+
+    public StackQueue() {
+        this.s1 = new Stack<>();
+    }
+
+    public void enqueue(T data) {
+        s1.push(data);
+        s1.size++;
+        if (s1.size() == 1) {
+            s2.push(data);
+        } else {
+            Stack temp = s1;
+            for (int i = 0; i < s1.size() - 1; i++) {
+                s2.push(temp.pop());
+            }
+        }
+
+    }
+
 }
