cea4adca7ac134e05b05c19a5969158afad4b9a9
John Karmozyn <hughkarmozyn@icloud.com>
2023-11-30 14:28:50 -0700

    comments on code!

--- StackQueue/src/Main.java
+++ StackQueue/src/Main.java
@@ -1,28 +1,41 @@
+/*
+CZ
+12/4/23
+HK
+ */
 public class Main {
     public static void main(String[] args) {
         StackQueue<Integer> queue = new StackQueue<>();
 
+        // Enqueue three elements (1, 2, and 3) into the queue.
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
 
+// Print information about the current state of the queue.
         System.out.println("What is last in the queue?: " + queue.peek());
         System.out.println("What is the size of the queue?: " + queue.size());
         System.out.println("Is the queue empty?: " + queue.isEmpty());
 
+// Dequeue two elements from the queue and print them. Expected output: 1, 2
         System.out.println(queue.dequeue()); // Output: 1
         System.out.println(queue.dequeue()); // Output: 2
 
+// Print the updated size and emptiness status of the queue.
         System.out.println("What is the size of the queue?: " + queue.size());
         System.out.println("Is the queue empty?: " + queue.isEmpty());
 
+// Enqueue a new element (4) into the queue.
         queue.enqueue(4);
 
+// Dequeue the remaining elements from the queue and print them. Expected output: 3, 4
         System.out.println(queue.dequeue()); // Output: 3
         System.out.println(queue.dequeue()); // Output: 4
 
+// Print the final size and emptiness status of the queue.
         System.out.println("What is the size of the queue?: " + queue.size());
         System.out.println("Is the queue empty?: " + queue.isEmpty());
+
 
     }
 }
--- StackQueue/src/StackQueue.java
+++ StackQueue/src/StackQueue.java
@@ -1,51 +1,72 @@
+/*
+CZ
+12/4/23
+HK
+ */
 public class StackQueue<T> {
 
     Stack<T> s1;
     Stack<T> s2;
 
     public StackQueue() {
+        // Constructor initializes two stacks, s1 and s2.
         this.s1 = new Stack<>();
         this.s2 = new Stack<>();
     }
 
     public void enqueue(T data) {
+        // Enqueue operation: Push the data onto the first stack, s1.
         s1.push(data);
     }
 
-    public T dequeue(){
+    public T dequeue() {
+        // Dequeue operation:
+        // Transfer elements from s1 to s2 to reverse the order.
         while (!s1.isEmpty()) {
             s2.push(s1.pop());
         }
+        // If s2 is still empty, the queue is empty.
         if (s2.isEmpty()) {
             throw new IllegalStateException();
         }
+        // Pop the front element from s2.
         T data = s2.pop();
+        // Transfer elements back to s1 to restore the original order.
         while (!s2.isEmpty()) {
             s1.push(s2.pop());
         }
+        // Return the dequeued data.
         return data;
     }
 
     public boolean isEmpty() {
-        if (s1.isEmpty() && s2.isEmpty()) {
-            return true;
-        } else {
-            return false;
-        }
+        // Check if both stacks are empty to determine if the queue is empty.
+        return s1.isEmpty() && s2.isEmpty();
     }
 
     public int size() {
-        return s1.size;
+        // Return the size of the first stack, which represents the queue's size.
+        return s1.size();
     }
+
     public T peek() {
-        while(!s1.isEmpty()) {
+        // Peek operation:
+        // Transfer elements from s1 to s2 to reverse the order.
+        while (!s1.isEmpty()) {
             s2.push(s1.pop());
         }
-        if(s2.isEmpty()) throw new IllegalStateException("Queue is empty");
+        // If s2 is empty, the queue is empty.
+        if (s2.isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        // Get the front element without removing it.
         T data = s2.peek();
-        while(!s2.isEmpty()) {
+        // Transfer elements back to s1 to restore the original order.
+        while (!s2.isEmpty()) {
             s1.push(s2.pop());
         }
+        // Return the peeked data.
         return data;
     }
+
 }
